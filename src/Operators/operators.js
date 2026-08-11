996f8e7946b8279dc1abfd634271384b9c41ec72
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-11 18:10:49 +0530

    Add Operators concepts

---
+++ src/Operators/operators.js
@@ -0,0 +1,49 @@
+// Arithmetic Operators
+
+let a=10;
+let b= 7;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+
+// Uniary Operators
+
+// console.log(a++);
+// console.log(++a);
+
+
+// assignment Operators
+
+b=a;
+console.log(b);
+
+// copmarison Operators
+
+
+console.log("==  (Equal to)");
+console.log(5 == "5");      // true
+
+console.log("\n=== (Strict Equal)");
+console.log(5 === "5");     // false
+
+console.log("\n!=  (Not Equal)");
+console.log(5 != 6);        // true
+
+console.log("\n!== (Strict Not Equal)");
+console.log(5 !== "5");     // true
+
+console.log("\n>   (Greater Than)");
+console.log(10 > 5);        // true
+
+console.log("\n<   (Less Than)");
+console.log(5 < 10);        // true
+
+console.log("\n>=  (Greater Than or Equal)");
+console.log(10 >= 10);      // true
+
+console.log("\n<=  (Less Than or Equal)");
+console.log(5 <= 10);       // true
