a04e9fc9c9ac5e77efa85d5d404142c90d614b1f
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-11 17:52:46 +0530

    Add Templete literals

---
+++ src/Templete Literals/templete.js
@@ -0,0 +1,9 @@
+let pencilprice=10;
+let eraserprice=5;
+// console.log("Total price is : " + ( pencilprice + eraserprice) + "Rupees.");
+
+//  Bag ticks
+
+let output = `The total price is: ${pencilprice + eraserprice} Rupees.`;
+
+console.log(output);
