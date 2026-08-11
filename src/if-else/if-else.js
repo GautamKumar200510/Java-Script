2ca9ded2b26a130a6e3aed2a0950f3f43841b4be
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-11 18:28:00 +0530

    Add if-else concepts

---
+++ src/if-else/if-else.js
@@ -0,0 +1,10 @@
+// conditional Statement
+console.log("before my if statement");
+let age=3;
+if(age>=18) {
+    console.log(" you can vote");
+}
+if(age<18) {
+    console.log(" you cannot vote");
+}
+console.log("after my if statement");
