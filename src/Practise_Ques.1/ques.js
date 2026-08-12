7c44a7c0c3210597529ec9e33f4f2884ec357acb
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-12 11:46:02 +0530

    Practise Question 1

---
+++ src/Practise_Ques.1/ques.js
@@ -0,0 +1,14 @@
+let color="green";
+
+// Traffic light system
+
+if(color== "red"){
+    console.log("stop!!");
+}
+if(color== "yellow"){
+    console.log("slow down!!");
+}
+if(color== "green"){
+    console.log("Go!!");
+}
+
