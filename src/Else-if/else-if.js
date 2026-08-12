eb4de02344b84571d28d95e8b950a861896d9b79
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-12 11:58:07 +0530

    Add else if concepts

---
+++ src/Else-if/else-if.js
@@ -0,0 +1,14 @@
+let marks=60;
+
+if (marks>=80){
+    console.log("A+");
+}
+else if (marks>=60){
+    console.log("B+");
+}
+else if (marks>=50){
+    console.log("C");
+}
+else if (marks>=30){
+    console.log("fail");
+}
