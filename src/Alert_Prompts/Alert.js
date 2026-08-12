71604befa108a9a9f4b81a13a4a0c4ce6c782fcf
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-12 13:12:05 +0530

    Add Alert and prompts concepts

---
+++ src/Alert_Prompts/Alert.js
@@ -0,0 +1,13 @@
+console.log("This  is a message");
+console.error("an error occur");
+console.warn("this is an warning");
+
+// prompt("enter your name :");
+
+// let firstname = prompt("Enter your first name");
+// console.log(firstname);
+
+let firstname = prompt("Enter your first name");
+console.log(firstname);
+let lastname = prompt("Enter your last name");
+console.log("Welcome" , firstname," " ,lastname, " !");
