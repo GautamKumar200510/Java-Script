b531509d7298a8a365bb43af87d3f2b7a7f615cf
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-12 12:04:03 +0530

    Add else concepts

---
+++ src/Else Statement/else.js
@@ -0,0 +1,25 @@
+let age= 9;
+if(age>=18){
+    console.log("you can vote");
+}
+
+else{
+    console.log("you cannot vote");
+}
+
+let color="blue";
+
+// Traffic light system
+
+if(color== "red"){
+    console.log("stop!!");
+}
+if(color== "yellow"){
+    console.log("slow down!!");
+}
+if(color== "green"){
+    console.log("Go!!");
+}
+else{
+    console.log("traffic light broken");
+}
