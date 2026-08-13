249a15584185359d2295ffddaced014573be9d91
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-13 11:47:40 +0530

    Add String Method

---
+++ src/String Method/method.js
@@ -0,0 +1,16 @@
+// let msg= " he   llo  ";
+
+
+let password= prompt("set  your password");
+console.log(password.trim());
+
+
+let name= "apna college";
+let msg="error";
+
+
+console.log(msg.toUpperCase());
+
+
+let messg = "I love coding";
+console.log(messg.indexOf("coding"));
