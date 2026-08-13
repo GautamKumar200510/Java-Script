73de4e6729145027a988a79bb18e0c42f6d8160b
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-13 12:17:59 +0530

    Add Slice method concepts

---
+++ src/Slice Method/slice.js
@@ -0,0 +1,15 @@
+// let msg="hello";
+// // console.log(msg.slice(3 , msg.length));
+// console.log( msg.slice(-3));  // 5-1 => 4   llo
+
+// Replace Method
+
+let msg="i love coding";
+console.log(msg.replace("i"));
+console.log(msg.replace("i" , "you"));
+
+
+// Repeat Method
+
+let str="coding";
+console.log(str.repeat("5"));
