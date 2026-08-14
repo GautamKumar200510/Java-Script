dfb742116519268437ba07e1ef0ce9a3d5c1cb6a
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-14 22:50:26 +0530

    Add Array Methdos indexof & Includes

---
+++ src/Array_Method/Array_method.js
@@ -0,0 +1,28 @@
+// Array Methods
+
+let cars= ["Bmw", "Audi", "Suzuki", "Thar"];  
+console.log(cars);
+cars.push("Toyota");
+cars.push("Ferari");
+cars.pop();
+console.log(cars);
+cars.unshift("Mercidies");
+console.log(cars);
+cars.unshift("swift");
+console.log(cars);
+cars.shift();
+console.log(cars);
+
+
+
+
+// Array Indexof & Includes
+
+cars.indexOf("Audi");
+console.log(cars);
+
+cars.includes("Thar");
+console.log(cars);
+
+
+
