83c92d102a64f05971f7fda3ad46724d8e417ca4
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-14 23:15:26 +0530

    Add Sort in Array

--- src/Array_Method/Array_method.js
+++ src/Array_Method/Array_method.js
@@ -1,7 +1,10 @@
 // Array Methods
 
-let cars= ["Bmw", "Audi", "Suzuki", "Thar"];  
+let cars= ["Bmw", "Audi", "Suzuki", "Thar"]; 
+let bike =["Honda" , "Splender" , "Royal Enfield"  ]; 
 console.log(cars);
+console.log(bike);
+
 cars.push("Toyota");
 cars.push("Ferari");
 cars.pop();
@@ -25,4 +28,27 @@ cars.includes("Thar");
 console.log(cars);
 
 
+// Array Concatenation
+
+
+
+cars.concat(bike);
+
+bike.reverse();
+
+// Array Slice  & Splice
+
+cars.slice(2);
+
+cars.splice(1);
+
+bike.splice(1,3);
+
+// Sort in Array
+
+cars.sort();
+
+let day=["mon", "tue", "wed", "thur" ," frid" , "sat"];
+console.log(day);
+day.sort();
 
