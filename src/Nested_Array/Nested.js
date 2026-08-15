85601fd2db07c2035c6218b09196ddf807d9d89b
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-15 23:28:58 +0530

    Add Todo  projects

--- src/Nested_Array/Nested.js
+++ src/Nested_Array/Nested.js
@@ -1,3 +1,15 @@
-let nums=[[1,2], [3,4] , [5,6] ];
-console.log(nums);
-nums.length
+// let nums=[[1,2], [3,4] , [5,6] ];
+// console.log(nums);
+// nums.length
+
+
+// Nested Loop with Array
+
+let heroes =[["ironman" , "spiderman","thor"] , ["superman", "flash" , "Aqua Man"]]
+
+for (let i=0; i<heroes.length; i++){
+    console.log(i,heroes[i], heroes[i].length);
+    for(let j=0; j<heroes[i].length; j++){
+        console.log('j=${j} , ${heroes[i][j]}');
+    }
+}
