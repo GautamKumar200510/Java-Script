a2892446749655f8f649225259dec64496d4ad4e
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-15 16:20:40 +0530

    Add Loops concepts

--- src/For_Loop/loop.js
+++ src/For_Loop/loop.js
@@ -23,6 +23,21 @@
 
 // Print all even number(2 to 20)
 
-for(let i=2; i<=20; i=i+2){
- console.log(i);
+// for(let i=2; i<=20; i=i+2){
+//  console.log(i);
+// }
+
+
+//  Print the multiplication table for 5
+
+let n= prompt("Write your number");
+n= parseInt(n);
+
+for(let i=n; i<=n*10; i=i+n){
+    console.log(i);
 }
+
+// for(let i=5; i<=50; i=i+5 ){
+//     console.log(i);
+// }
+
