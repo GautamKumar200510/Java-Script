f030ac986127ba539f01f546ec6d547ce160f974
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-15 16:10:40 +0530

    Add For loops

---
+++ src/For_Loop/loop.js
@@ -0,0 +1,28 @@
+// for(let i=1; i<=5; i++){
+// console.log(i);
+// }
+
+
+// for(let j=10; j>=1; j=j-2){
+// console.log(j);
+// }
+
+
+// Print all odd numbers (1 to 15)
+
+
+// for(let i=1; i<=15; i=i+2){
+//     console.log(i);
+// }
+
+// console.log("backwords");
+
+// for(let i=1; i<=15; i=i-2){
+//     console.log(i);
+// }
+
+// Print all even number(2 to 20)
+
+for(let i=2; i<=20; i=i+2){
+ console.log(i);
+}
