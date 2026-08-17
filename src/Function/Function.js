3687cadc74a742fe8f697ef1d94310bb01ad1e7f
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-17 15:29:04 +0530

    Add Function

---
+++ src/Function/Function.js
@@ -0,0 +1,21 @@
+function hello () {
+    console.log("hello");
+
+}
+
+function printname(){
+    console.log("Gautam Kumar");
+    console.log("Learning from apna college");
+
+
+}
+
+function print1to5(){
+    for(let i=1; i<=5; i++){
+        console.log(i);
+    }
+}
+
+print1to5();
+
+// printname();
