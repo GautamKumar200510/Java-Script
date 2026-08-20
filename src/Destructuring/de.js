6324b7d65937f566dc7b341bb6bed206a79b2ad6
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-20 08:54:53 +0530

    Add DeStructuring

---
+++ src/Destructuring/de.js
@@ -0,0 +1,7 @@
+let names=["gautam", "Brishal", "Aman","Rohit", "shk", "skjk", "ccnmb"];
+// let winner = names[0];
+// let runnerup = names[1];
+// let secondrunnerup = names[2];
+
+
+let [winner, runnerup, ...others] = names;
