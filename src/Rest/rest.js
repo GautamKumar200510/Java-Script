169445f99bd6df59807dcd36339d465b5147728d
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-20 08:47:17 +0530

    Add Rest

---
+++ src/Rest/rest.js
@@ -0,0 +1,14 @@
+function sum(...args){
+    return args.reduce((sum,el) => sum +el);
+}
+
+function min(msg, ...args){
+    console.log(msg);
+    return args.reduce((min , el) =>{
+        if(min>el){
+            return el;
+        }else{
+            return min;
+        }
+    });
+}
