a807cccaf459c52cf159c10f3a283e1703bca039
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-23 23:38:18 +0530

    Add Event Listener for the element

--- src/DOM Event/Event.js
+++ src/DOM Event/Event.js
@@ -27,24 +27,37 @@
 // EventListener
 
 
-let btn = document.querySelector("button");
+// let btn = document.querySelector("button");
 
-for(btn of btns){
-    // btn.oneclick = sayHello;
-    //  btn.oneclick = sayName;
+// for(btn of btns){
+//     // btn.oneclick = sayHello;
+//     //  btn.oneclick = sayName;
 
-    btn.addEventListener("click", sayHello);
-     btn.addEventListener("click", sayName);
+//     btn.addEventListener("click", sayHello);
+//      btn.addEventListener("click", sayName);
   
-}
+// }
 
 
-function sayHello(){
-       alert("Hello!");
-}
+// function sayHello(){
+//        alert("Hello!");
+// }
 
-function sayName(){
-       alert("Apna College");
-}
+// function sayName(){
+//        alert("Apna College");
+// }
+
+
+// Event Listener for the element
+
+let p = document.querySelector("p");
+
+p.addEventListener("click" , function (){
+    console.log("parah was clicked");
+});
+
+let box = document.querySelector("mouseenter", function () {
+    console.log("mouse inside div");
+});
 
 
