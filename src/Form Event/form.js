fbe196dd61c4fc43fa1b6ec257e412f8371ad268
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-24 12:09:28 +0530

    Add Extracting Form Data

--- src/Form Event/form.js
+++ src/Form Event/form.js
@@ -1,7 +1,29 @@
+// let form = document.querySelector("form");
+
+// form.addEventListener("submit", function (){
+//     // console.log("Form submission");
+//     alert("Form submission");
+
+// });
+
+
+// Extracting Form Data
+
 let form = document.querySelector("form");
 
-form.addEventListener("submit", function (){
-    // console.log("Form submission");
-    alert("Form submission");
+form.addEventListener("submit", function (event){
+  event.preventDefault();
+  console.dir(form);
+
+//   let user = document.querySelector("#user");
+//     let password = document.querySelector("#password");
+
+  let user = this.elements[0];
+  let password = this.elements[1];
+
+  console.log(user.value);
+  console.log(password.value);
+
+  alert('Hi ${user.value}, your password is set to ${pass.value}');
 
 });
