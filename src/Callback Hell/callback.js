cca4cb3c105e51a3ed78cf5b018e5b317e1a599d
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-26 16:21:51 +0530

    Add promises

--- src/Callback Hell/callback.js
+++ src/Callback Hell/callback.js
@@ -30,3 +30,37 @@ changeColor("red", 1000, () => {
 });
 
 // callbacks nesting -> callback hell
+
+
+
+// promises
+
+saveToDb(
+    "apna college",
+    () => {
+        console.log("success1 : your data was saved");
+
+        saveToDb(
+            "hello world",
+            () => {
+                console.log("success2: data2 saved");
+
+                saveToDb(
+                    "shradha",
+                    () => {
+                        console.log("success3: data3 saved");
+                    },
+                    () => {
+                        console.log("failure3 : weak connection");
+                    }
+                );
+            },
+            () => {
+                console.log("failure2 : weak connection");
+            }
+        );
+    },
+    () => {
+        console.log("failure1 : weak connection");
+    }
+);
