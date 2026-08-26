3caf9e97402b0d253bf535917ab698abc3f0ba18
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-26 16:42:45 +0530

    Add Result Error in the promises

--- src/promises/promises.js
+++ src/promises/promises.js
@@ -17,3 +17,42 @@ saveToDb("gautam Kumar")
     .catch(() => {
         console.log("promise was rejected");
     });
+
+
+    // Promises Chaining
+
+    // saveToDb("Gautam Kumar")
+    // .then(() => {
+    //     console.log("data1 saved");
+    //     return saveToDb("helloworld");
+    // })
+    // .then(() => {
+    //     console.log("data2 saved");
+    // })
+    // .catch(() => {
+    //     console.log("promise was rejected");
+    // });
+
+
+    // Result error in promises
+
+    saveToDb("apna college")
+    .then((result) => {
+        console.log("data1 saved");
+        console.log("result of promise: ", result);
+        return saveToDb("helloworld");
+    })
+    .then((result) => {
+        console.log("data2 saved");
+        console.log("result of promise: ", result);
+        return saveToDb("shradha");
+    })
+    .then((result) => {
+        console.log("data3 saved");
+        console.log("result of promise: ", result);
+    })
+    .catch((error) => {
+        console.log("promise was rejected");
+        console.log("error of promise: ", error);
+    });
+
