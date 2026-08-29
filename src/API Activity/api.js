90977c35ee162825db0b042ca464b99a779bc9b1
GautamKumar200510 <gautamkr.71019@gmail.com>
2026-08-29 21:12:14 +0530

    Add API Activity

---
+++ src/API Activity/api.js
@@ -0,0 +1,18 @@
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+    getColleges();
+});
+
+let country = "nepal";
+
+async function getColleges() {
+    try {
+        let res = await axios.get(url + country);
+        console.log(res);
+    } catch (e) {
+        console.log("error : ", e);
+        return [];
+    }
+}
